42e785ecca97d327afa41b68db42f8805f131570
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-03 00:53:38 +0200

    refacto : delete useless function for more simplicity

--- src/module-1/01-fizz-buzz/fizzbuzz.ts
+++ src/module-1/01-fizz-buzz/fizzbuzz.ts
@@ -1,15 +1,11 @@
-const isMultiple = (inputNumber :number, multipleNumber :number) => {
-    return (inputNumber % multipleNumber) === 0;
-}
-
 const isMultipleOf3 = (inputNumber :number) => {
     const MULTIPLE_OF_3 = 3;
-    return isMultiple(inputNumber, MULTIPLE_OF_3);
+    return (inputNumber % MULTIPLE_OF_3) === 0;
 }
 
 const isMultipleOf5 = (inputNumber :number) => {
     const MULTIPLE_OF_5 = 5;
-    return isMultiple(inputNumber, MULTIPLE_OF_5);
+    return (inputNumber % MULTIPLE_OF_5) === 0;
 }
 
 export function fizzBuzz(input: number) : number | string {
