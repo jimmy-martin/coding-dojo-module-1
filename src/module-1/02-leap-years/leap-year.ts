5cd9878c7a45130523451c232050b78f16ccd2b0
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-03 16:46:07 +0200

    some practice without correction

--- src/module-1/02-leap-years/leap-year.ts
+++ src/module-1/02-leap-years/leap-year.ts
@@ -1,3 +1,11 @@
-export function isLeap() {
+export function isLeap(input :number) : boolean {
+    if((input % 400) == 0){
+        return true
+    }
+    if((input % 4) == 0 && (input % 100) != 0){
+        return true;
+    }
+
+    return false;
 }
 
--- src/module-1/02-leap-years/leap-years.spec.ts
+++ src/module-1/02-leap-years/leap-years.spec.ts
@@ -1,10 +1,29 @@
 import { isLeap } from "./leap-year";
 
-xdescribe('LeapYear', () => {
-    test('isLeap should returns xxx when year is xxx', () => {
+describe('LeapYear', () => {
+    test('isLeap should returns false when year is 2001', () => {
         // Arrange
+        const inputYear = 2001;
+
+        const expected = false;
         // Act
+
+        const result = isLeap(inputYear);
+
         // Assert
-        expect(false).toBe(true)
+        expect(result).toBe(expected)
+    })
+
+    test('isLeap should returns true when year is 1996', () => {
+        // Arrange
+        const inputYear = 1996;
+
+        const expected = true;
+        // Act
+
+        const result = isLeap(inputYear);
+
+        // Assert
+        expect(result).toBe(expected)
     })
 })
