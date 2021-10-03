81fc6de0834bcd0d68651dbcd40525d22cbdd551
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-03 16:52:37 +0200

    refacto

--- src/module-1/02-leap-years/leap-year.ts
+++ src/module-1/02-leap-years/leap-year.ts
@@ -1,8 +1,23 @@
-export function isLeap(input :number) : boolean {
-    if((input % 400) == 0){
+const isMultipleOf400 = (inputYear :number) => {
+    const MULTIPLE_OF_400 = 400;
+    return (inputYear % MULTIPLE_OF_400) === 0;
+}
+
+const isMultipleOf4 = (inputYear :number) => {
+    const MULTIPLE_OF_4 = 4;
+    return (inputYear % MULTIPLE_OF_4) === 0;
+}
+
+const isMultipleOf100 = (inputYear :number) => {
+    const MULTIPLE_OF_100 = 100;
+    return (inputYear % MULTIPLE_OF_100) === 0;
+}
+
+export function isLeap(inputYear :number) : boolean {
+    if(isMultipleOf400(inputYear)){
         return true
     }
-    if((input % 4) == 0 && (input % 100) != 0){
+    if(isMultipleOf4(inputYear) && !isMultipleOf100(inputYear)){
         return true;
     }
 
--- src/module-1/02-leap-years/leap-years.spec.ts
+++ src/module-1/02-leap-years/leap-years.spec.ts
@@ -26,4 +26,17 @@ describe('LeapYear', () => {
         // Assert
         expect(result).toBe(expected)
     })
+
+    test('isLeap should returns false when year is 1900', () => {
+        // Arrange
+        const inputYear = 1900;
+
+        const expected = false;
+        // Act
+
+        const result = isLeap(inputYear);
+
+        // Assert
+        expect(result).toBe(expected)
+    })
 })
